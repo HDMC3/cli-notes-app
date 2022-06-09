3bde0874372b5e34d9ceb4d4391b5fb3cf552305
Hugo Moran <moranh56@gmail.com>
2022-06-09 15:33:12 -0600

    Add new styles to View Headers

--- Cli/Common/Helpers.cs
+++ Cli/Common/Helpers.cs
@@ -4,7 +4,10 @@ namespace Cli.Common {
     public class Helpers {
         public static void WriteRuleWidget(string title) {
             AnsiConsole.WriteLine();
-            var rule = new Rule(title);
+            title = title.Replace("|", "[#d3dd57]|[/]");
+            var rule = new Rule($"[white]{title}[/]");
+            rule.Style = Style.Parse("#d3dd57");
+            rule.Border(BoxBorder.Double);
             rule.Alignment = Justify.Left;
             AnsiConsole.Write(rule);
             AnsiConsole.WriteLine();
